0dffbb931edcd0d12fa4b768d5fba3ca65256b56
CaQtiml <lsivakorn@gmail.com>
2022-10-13 12:12:10 +0700

    refactor codes

--- demo_2/server.py
+++ demo_2/server.py
@@ -6,22 +6,7 @@ from game import Game
 server = "192.168.1.53"
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
-s.listen(2)
-print("Waiting for a connection, Server Started")
-
-connected = set()
-games = {}
-idCount = 0
-
-
-def threaded_client(conn, p, gameId):
+def threaded_client(conn, p, gameId, games):
     global idCount
     conn.send(str.encode(str(p)))
 
@@ -56,21 +41,32 @@ def threaded_client(conn, p, gameId):
     idCount -= 1
     conn.close()
 
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.bind((server, port))
+    except socket.error as e:
+        str(e)
+
+    s.listen(2)
+    print("Waiting for a connection, Server Started")
+
+    games = {}
+    idCount = 0
+    while True:
+        conn, addr = s.accept()
+        print("Connected to:", addr)
+
+        idCount += 1
+        p = 0
+        gameId = (idCount - 1)//2
+        if idCount % 2 == 1:
+            games[gameId] = Game(gameId)
+            print("Creating a new game...")
+        else:
+            games[gameId].ready = True
+            p = 1
 
 
-while True:
-    conn, addr = s.accept()
-    print("Connected to:", addr)
-
-    idCount += 1
-    p = 0
-    gameId = (idCount - 1)//2
-    if idCount % 2 == 1:
-        games[gameId] = Game(gameId)
-        print("Creating a new game...")
-    else:
-        games[gameId].ready = True
-        p = 1
-
-
-    start_new_thread(threaded_client, (conn, p, gameId))
+        start_new_thread(threaded_client, (conn, p, gameId, games))
